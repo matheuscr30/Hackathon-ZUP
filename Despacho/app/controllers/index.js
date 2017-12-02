e74c27fcd7ae585e4eeaeecebe338687a74e3a4d
matheuscr30 <matheuscunhareis30@gmail.com>
2017-12-01 23:50:16 -0200

    sdfsd

--- Despacho/app/controllers/index.js
+++ Despacho/app/controllers/index.js
@@ -1,3 +1,11 @@
 module.exports.home = function (application, req, res) {
     res.render('index');
 }
+
+module.exports.getOcorrencias = function (application, req, res) {
+    res.send('getOcorrencias');
+}
+
+module.exports.getRecursos = function (application, req, res) {
+    res.send('getRecursos');
+}
--- Despacho/app/routes/index.js
+++ Despacho/app/routes/index.js
@@ -2,4 +2,16 @@ module.exports = function (application) {
     application.get('/', function (req, res) {
         application.app.controllers.index.home(application, req, res);
     });
+
+    application.post('/ocorrencias', function (req, res) {
+        application.app.controllers.index.getOcorrencias(application, req, res);
+    });
+
+    application.post('/recursos', function (req, res) {
+        application.app.controllers.index.getRecursos(application, req, res);
+    });
+
+    application.get('/chamada', function (req, res) {
+        res.send('');    
+    });
 }
